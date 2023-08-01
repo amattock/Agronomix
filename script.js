4861b1ab4f08b948813bb324a3aac2c4566a47e2
amattock <134568545+amattock@users.noreply.github.com>
2023-07-31 20:31:39 -0500

    added list of Poly API to JS

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 
 
-var repoList = document.querySelector('ul');
-var fetchButton = document.getElementById('fetch-button');
+// var repoList = document.querySelector('ul');
+// var fetchButton = document.getElementById('fetch-button');
 
-function getApi() {
-  // replace `octocat` with anyone else's GitHub username
-  var requestUrl = 'http://api.agromonitoring.com/agro/1.0/polygons/5abb9fb82c8897000bde3e87?appid=test';
+// function getApi() {
+//   // replace `octocat` with anyone else's GitHub username
+//   var requestUrl = 'http://api.agromonitoring.com/agro/1.0/polygons/5abb9fb82c8897000bde3e87?appid=test';
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = data[i].html_url;
-        repoList.appendChild(listItem);
-      }
-    });
-}
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       for (var i = 0; i < data.length; i++) {
+//         var listItem = document.createElement('li');
+//         listItem.textContent = data[i].html_url;
+//         repoList.appendChild(listItem);
+//       }
+//     });
+// }
 
-fetchButton.addEventListener('click', getApi);
-
+// fetchButton.addEventListener('click', getApi);
+ 
 
 
 
@@ -81,3 +81,23 @@ modalCloseBtn.addEventListener("click", function(){
 
 });
 
+// Alex's add - "Get list of Polys"
+const apiKey = '5377301dcdca71537669d26ce2c115d4';
+const apiUrl = 'https://api.agromonitoring.com/agro/1.0';
+
+const getListOfPolygons = async () => {
+ try {
+   const response = await fetch(`${apiUrl}/polygons?appid=${apiKey}`);
+
+   if (!response.ok) {
+     throw new Error('Failed to fetch the list of polygons');
+   }
+
+   const data = await response.json();
+   console.log('List of polygons:', data);
+ } catch (error) {
+   console.error('Error fetching the list of polygons:', error.message);
+ }
+};
+
+getListOfPolygons();
